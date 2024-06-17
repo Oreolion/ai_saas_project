cbd86de9ee5b6b157332163e4cf84a6032c13089
oreolion <remyoreo11@gmail.com>
2024-06-17 06:53:26 -0700

    add authentication and middleware with clerk_dev

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://pro-rat-74.clerk.accounts.dev
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://pro-rat-74.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
