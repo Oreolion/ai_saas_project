216f8c3b904d155bd468ea4e622935daac8a666b
oreolion <remyoreo11@gmail.com>
2024-09-10 03:35:39 -0700

    minor fix;

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,77 +1,80 @@
-import React, { useCallback } from 'react'
-import { EmblaOptionsType, EmblaCarouselType } from 'embla-carousel'
-import { DotButton, useDotButton } from './EmblaCarouselDotButton'
-import Autoplay from 'embla-carousel-autoplay'
-import useEmblaCarousel from 'embla-carousel-react'
-import { CarouselProps } from '@/types'
-import { useRouter } from 'next/navigation'
-import Image from 'next/image'
-import LoaderSpinner from './LoaderSpinner'
+import React, { useCallback } from "react";
+import { EmblaOptionsType, EmblaCarouselType } from "embla-carousel";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import { CarouselProps } from "@/types";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import LoaderSpinner from "./LoaderSpinner";
 
-
-
-const Carousel = ({ fansLikeDetail}: CarouselProps) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()])
-    const router = useRouter()
+const Carousel = ({ fansLikeDetail }: CarouselProps) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  const router = useRouter();
   const onNavButtonClick = useCallback((emblaApi: EmblaCarouselType) => {
-    const autoplay = emblaApi?.plugins()?.autoplay
-    if (!autoplay || !("stopOnInteraction" in autoplay.options)) return
+    const autoplay = emblaApi?.plugins()?.autoplay;
+    if (!autoplay || !("stopOnInteraction" in autoplay.options)) return;
 
     const resetOrStop =
       autoplay.options.stopOnInteraction === false
-        ? autoplay.reset as () => void
-        : autoplay.stop as () => void
+        ? (autoplay.reset as () => void)
+        : (autoplay.stop as () => void);
 
-    resetOrStop()
-  }, [])
+    resetOrStop();
+  }, []);
 
   const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(
     emblaApi,
     onNavButtonClick
-  )
+  );
 
-  const slides = fansLikeDetail && fansLikeDetail?.filter((item: any)=> item.totalPodcasts > 0)
+  const slides =
+    fansLikeDetail &&
+    fansLikeDetail?.filter((item: any) => item.totalPodcasts > 0);
 
-  if (!slides) return <LoaderSpinner></LoaderSpinner>
+  if (!slides) return <LoaderSpinner></LoaderSpinner>;
 
   return (
-    <section className="flex w-full flex-col gap-4 overflow-hidden " 
-    // ref={emblemRef}
+    <section
+      className="flex w-full flex-col gap-4 overflow-hidden "
+      ref={emblaRef}
     >
-        <div className='flex'>
+      <div className="flex">
         {slides.slice(0, 5).map((item) => (
           <figure
             key={item._id}
             className="carousel_box"
-            onClick={() => router.push(`/podcasts/${item.podcast[0]?.podcastId}`)}
+            onClick={() =>
+              router.push(`/podcasts/${item.podcast[0]?.podcastId}`)
+            }
           >
-            <Image 
-            src={item.imageUrl}
-            alt="card"
-            fill
-            className="absolute size-full rounded-xl border-none"
+            <Image
+              src={item.imageUrl}
+              alt="card"
+              fill
+              className="absolute size-full rounded-xl border-none"
             />
             <div className="glassmorphism-black relative z-10 flex flex-col rounded-b-xl p-4">
-              <h2 className="text-14 font-semibold text-white-1">{item.podcast[0]?.podcastTitle}</h2>
+              <h2 className="text-14 font-semibold text-white-1">
+                {item.podcast[0]?.podcastTitle}
+              </h2>
               <p className="text-12 font-normal text-white-2">{item.name}</p>
             </div>
           </figure>
-        ))}            
-        </div>
+        ))}
+      </div>
 
-     
-        <div className="flex justify-center gap-2">
+      <div className="flex justify-center gap-2">
         {scrollSnaps.map((_, index) => (
           <DotButton
             key={index}
             onClick={() => onDotButtonClick(index)}
             selected={index === selectedIndex}
-            />
-          ))}
+          />
+        ))}
       </div>
-     
     </section>
-  )
-}
+  );
+};
 
 export default Carousel;
